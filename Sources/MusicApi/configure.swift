9c8aa6b34645054a787d0865862f0c164dd61574
Kuripipeer <mchristianaxel@gmail.com>
2025-05-28 19:51:34 -0600

    fix

--- Sources/MusicApi/configure.swift
+++ Sources/MusicApi/configure.swift
@@ -9,11 +9,11 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "db-mysql-sfo3-61367-do-user-22002422-0.g.db.ondigitalocean.com",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 25060,
-        username: Environment.get("DATABASE_USERNAME") ?? "doadmin",
-        password: Environment.get("DATABASE_PASSWORD") ?? "AVNS_Za6yFeuNrZozRCwxJpP",
-        database: Environment.get("DATABASE_NAME") ?? "music_db",
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 3310,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
         tlsConfiguration: .forClient(certificateVerification:.none)
     ), as: .mysql)
 
