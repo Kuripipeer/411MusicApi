31ed2843ba5b8a7d31fadc5adb0e3df195eea21c
Kuripipeer <mchristianaxel@gmail.com>
2025-06-09 15:12:06 -0600

    Prueba git

--- Sources/MusicApi/Migrations/UpdateAlbum.swift
+++ Sources/MusicApi/Migrations/UpdateAlbum.swift
@@ -1,11 +1,10 @@
 import Fluent
 
-struct UpdateArtist: AsyncMigration {
-    func prepare(on database: any Database) async throws {
+struct UpdateAlbum: AsyncMigration {
+    func prepare(on database: any FluentKit.Database) async throws {
         try await database.schema("albums")
-            .id()
             .field("year", .string)
-            .create()
+            .update()
     }
 
     func revert(on database: any Database) async throws {
